3525d07de98f7e491f9a861813b6ca8611c348a2
Q-shui <73942545+Q-shui@users.noreply.github.com>
2021-10-19 23:10:17 +0800

    Add files via upload

---
+++ 相同的树_test_10_18/相同的树_test_10_18/test.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+struct TreeNode
+{
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+};
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q);
+
+int main()
+{
+
+
+	return 0;
+}
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q)
+{
+	if (p == NULL && q == NULL)
+		return true;
+
+	if (!(p && q))
+		return false;
+
+	if (p->val != q->val)
+		return false;
+
+	return isSameTree(p->left, q->left) &&
+		isSameTree(p->right, q->right);
+}
